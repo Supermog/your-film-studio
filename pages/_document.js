a31c9e2fb47ca4de267352740cd467279a7bda0b
Brian <brian67531@gmail.com>
2023-02-15 15:04:40 +0100

    Made footer always be at the bottom of the page

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,18 @@ import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 
 export default function Document() {
+
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
-        <NavBar />
-        <Main />
-        <Footer />
+        <div className={`flex flex-col justify-between min-h-screen`}>
+          <div>
+            <NavBar />
+            <Main />
+          </div>
+          <Footer />
+        </div>
         <NextScript />
       </body>
     </Html>
